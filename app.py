36bc29f41a6bea975221244c18c5288dea659f34
Miguel dos Reis <migueldreis01@gmai.com>
2023-01-15 19:11:01 -0300

    Export logs and integrate with Loki

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import logging
+
 from opentelemetry import trace
 from opentelemetry import metrics
+from opentelemetry import _logs as log
+from opentelemetry._logs import set_logger_provider
 
 from random import randint
 from flask import Flask, request
@@ -10,6 +14,13 @@ from opentelemetry.sdk.metrics import MeterProvider
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
+from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
+from opentelemetry.exporter.otlp.proto.grpc._log_exporter import (
+    OTLPLogExporter,
+)
+
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 
 app = Flask(__name__)
@@ -30,6 +41,15 @@ processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="http://otel_collector:
 provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 
+logger_provider = LoggerProvider(resource=resource)
+set_logger_provider(logger_provider)
+
+exporter = OTLPLogExporter(endpoint="http://otel_collector:4317", insecure=True)
+logger_provider.add_log_record_processor(BatchLogRecordProcessor(exporter))
+handler = LoggingHandler(level=logging.NOTSET, logger_provider=logger_provider)
+
+# Attach OTLP handler to root logger
+logging.getLogger().addHandler(handler)
 
 tracer = trace.get_tracer(__name__)
 # Acquire a meter.
@@ -44,14 +64,20 @@ roll_counter = meter.create_counter(
 # Start Prometheus client
 start_http_server(port=8000, addr="0.0.0.0")
 
+
 @app.route("/rolldice")
 def roll_dice():
     return str(do_roll())
 
+
 def do_roll():
-    with tracer.start_as_current_span("do_roll") as rollspan:  
+    with tracer.start_as_current_span("do_roll") as rollspan:
         res = randint(1, 6)
         rollspan.set_attribute("roll.value", res)
         # This adds 1 to the counter for the given roll value
         roll_counter.add(1, {"roll.value": res})
+
+        logger = logging.getLogger("do_roll_endpoint")
+        logger.info(f"Dice rolled with value: {res}")
+
         return res
